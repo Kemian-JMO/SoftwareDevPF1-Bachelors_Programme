8f30dd0eab8264409d390b45782086ad40131bd5
KemKillian <Jonas@olsson-web.dk>
2026-02-27 15:39:39 +0100

    Updated Project class to

--- src/Project.java
+++ src/Project.java
@@ -1,2 +1,12 @@
 public class Project extends StudyActivity{
+    public Project(String name, int ects, Programme programme, String id) {
+        super(name, ects, programme, id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Project p){
+            return this.getId().equals(p.getId());
+        }else return false;
+    }
 }
