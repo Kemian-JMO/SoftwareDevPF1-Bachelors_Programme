a4fdce9130b8ec515a528a60797e162a0f505821
KemKillian <Jonas@olsson-web.dk>
2026-02-27 15:40:00 +0100

    Updated SubjectModule to include

--- src/SubjectModule.java
+++ src/SubjectModule.java
@@ -1,2 +1,12 @@
 public class SubjectModule extends Programme{
+    public SubjectModule(String name, String id, String semester) {
+        super(name, id, semester);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof SubjectModule s){
+            return this.getId().equals(s.getId());
+        }else return false;
+    }
 }
