8afcdbcea0956634533fc843b9446f7d741b81c0
KemKillian <Jonas@olsson-web.dk>
2026-02-27 15:40:09 +0100

    Updated BachelorProgramme class to

--- src/BachelorsProgramme.java
+++ src/BachelorsProgramme.java
@@ -1,2 +1,39 @@
+import java.util.ArrayList;
+
 public class BachelorsProgramme {
+    private boolean isCompleted = false;
+    private ArrayList<StudyActivity> studyActivities = new ArrayList<>();
+    private Student student = null;
+    private int ects
+
+    public BachelorsProgramme() {
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted() {
+        int ectsAmount = 0;
+        for (StudyActivity studyActivity : studyActivities) {
+            ectsAmount += studyActivity.getEcts();
+        }
+        return ectsAmount ==
+    }
+
+    public ArrayList<StudyActivity> getStudyActivities() {
+        return studyActivities;
+    }
+
+    public void setStudyActivities(ArrayList<StudyActivity> studyActivities) {
+        this.studyActivities = studyActivities;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
 }
