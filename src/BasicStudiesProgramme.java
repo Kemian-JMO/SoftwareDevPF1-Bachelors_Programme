e84b689078c0d0379af4a47d4feab51c3b3e41ef
KemKillian <Jonas@olsson-web.dk>
2026-02-27 15:39:02 +0100

    Updated BasicStudiesProgramme.java

--- src/BasicStudiesProgramme.java
+++ src/BasicStudiesProgramme.java
@@ -1,2 +1,12 @@
 public class BasicStudiesProgramme extends Programme{
+    public BasicStudiesProgramme(String name, String id, String semester) {
+        super(name, id, semester);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof BasicStudiesProgramme b){
+            return this.getId().equals(b.getId());
+        } else return false;
+    }
 }
