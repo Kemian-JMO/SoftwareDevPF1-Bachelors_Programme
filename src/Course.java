b38fcfa156c4f1cd89d42338f9dab5fb828141e9
KemKillian <Jonas@olsson-web.dk>
2026-02-27 15:39:15 +0100

    Updated Course class

--- src/Course.java
+++ src/Course.java
@@ -1,2 +1,14 @@
+import java.util.Objects;
+
 public class Course extends StudyActivity{
+    public Course(String name, int ects, Programme programme, String id) {
+        super(name, ects, programme, id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Course c){
+            return Objects.equals(this.getId(), c.getId());
+        } else return false;
+    }
 }
