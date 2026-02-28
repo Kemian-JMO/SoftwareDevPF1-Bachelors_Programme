ee9b3593a931fed1a7dbc7af0bb780e2dd323809
KemKillian <Jonas@olsson-web.dk>
2026-02-28 20:40:36 +0100

    updated things

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,11 @@
 public class Student extends Person{
+    priva
+    public Student(String email, String lastName, String firstName, String address) {
+        super(email, lastName, firstName, address);
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
 }
